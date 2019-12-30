42e1715975c7bfbc163f0cf583a009a7f287e9b6
Tim French <tfrench15@gmail.com>
2019-12-29 23:08:37 -0800

    remove build

--- day_2/target/debug/day_2-a3f888cad6d58de2.d
+++
@@ -1,1 +0,0 @@
-/Users/timfrench/aoc-2015/day_2/target/debug/day_2-a3f888cad6d58de2: /Users/timfrench/aoc-2015/day_2/src/input.txt /Users/timfrench/aoc-2015/day_2/src/main.rs
--- day_2/target/debug/day_2.d
+++
@@ -1,1 +0,0 @@
-/Users/timfrench/aoc-2015/day_2/target/debug/day_2: /Users/timfrench/aoc-2015/day_2/src/input.txt /Users/timfrench/aoc-2015/day_2/src/main.rs
