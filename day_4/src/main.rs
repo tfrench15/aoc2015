0fc7238909042254317bcb8aa7676523e27e7c55
Tim French <tfrench15@gmail.com>
2019-12-31 13:25:53 -0800

    day 4

---
+++ day_4/src/main.rs
@@ -0,0 +1,33 @@
+
+use md5;
+
+fn main() {
+    let input = "ckczppom".to_string();
+    let number = run(&input);
+
+    println!("number is {}", number);
+}
+
+fn compute_md5(num: u64, input: &str) -> md5::Digest {
+    let num_str = num.to_string();
+    let data = format!("{}{}", input, num_str);
+    let bytes = data.as_bytes();
+
+    md5::compute(bytes)   
+}
+
+fn run(input: &str) -> u64 {
+    let mut n = 1;
+
+    loop {
+        let digest = compute_md5(n, input);
+        let hash = format!("{:x}", digest);
+
+        if hash.starts_with("000000") {
+            break n;
+        }
+
+        n+= 1
+    }
+}
+
